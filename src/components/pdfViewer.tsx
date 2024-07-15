39ab49701c8da52a58f7ba9cedb4c55552b58a7a
stephenChinag <stephenchinag@gmail.com>
2024-07-15 15:32:24 +0100

    pdf-viewer-features-completed

--- src/components/pdfViewer.tsx
+++ src/components/pdfViewer.tsx
@@ -1,5 +1,8 @@
+import { Viewer, Worker } from "@react-pdf-viewer/core";
 import { ChangeEvent, useState } from "react";
-
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 export default function PdfViewer() {
   const [pdfFile, setPdfFile] = useState(null);
 
@@ -13,7 +16,7 @@ export default function PdfViewer() {
 
         reader.readAsDataURL(selectedFile);
         reader.onload = (e) => {
-          setPdfFile(e.target.result);
+          setPdfFile(e.target?.result);
         };
       } else {
         setPdfFile(null);
@@ -22,16 +25,36 @@ export default function PdfViewer() {
       console.log("please select A file");
     }
   };
+
+  const handleSubmit = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (pdfFile !== null) {
+      setViewPdf(pdfFile);
+    } else {
+      setViewPdf(null);
+    }
+  };
+
+  const newplugin = defaultLayoutPlugin();
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="file" onChange={handleChange} />
         <button type="submit"> View PDF</button>
       </form>
 
       <h2> View Pdf</h2>
 
-      <div className="pdf-container"></div>
+      <div className="pdf-container">
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+          {viewPdf && (
+            <>
+              <Viewer fileUrl={viewPdf} plugins={[newplugin]} />
+            </>
+          )}
+          {!viewPdf && <> No PDF</>}
+        </Worker>
+      </div>
     </div>
   );
 }
